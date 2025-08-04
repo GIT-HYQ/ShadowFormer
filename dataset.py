6434bff7357401b8c7a7f9d87078f26a18f7d8d7
hyq9003 <hyq9003@163.com>
2025-08-04 22:22:05 +0800

    add testset

--- dataset.py
+++ dataset.py
@@ -80,6 +80,51 @@ class DataLoaderVal(Dataset):
 
         self.target_transform = target_transform
 
+        gt_dir = 'val_C'
+        input_dir = 'val_A'
+        mask_dir = 'val_B'
+        
+        clean_files = sorted(os.listdir(os.path.join(rgb_dir, gt_dir)))
+        noisy_files = sorted(os.listdir(os.path.join(rgb_dir, input_dir)))
+        mask_files = sorted(os.listdir(os.path.join(rgb_dir, mask_dir)))
+
+
+        self.clean_filenames = [os.path.join(rgb_dir, gt_dir, x) for x in clean_files if is_png_file(x)]
+        self.noisy_filenames = [os.path.join(rgb_dir, input_dir, x) for x in noisy_files if is_png_file(x)]
+        self.mask_filenames = [os.path.join(rgb_dir, mask_dir, x) for x in mask_files if is_png_file(x)]
+
+
+        self.tar_size = len(self.clean_filenames)  
+
+    def __len__(self):
+        return self.tar_size
+
+    def __getitem__(self, index):
+        tar_index   = index % self.tar_size
+        
+
+        clean = torch.from_numpy(np.float32(load_img(self.clean_filenames[tar_index])))
+        noisy = torch.from_numpy(np.float32(load_img(self.noisy_filenames[tar_index])))
+        mask = load_mask(self.mask_filenames[tar_index])
+        mask = torch.from_numpy(np.float32(mask))
+
+        clean_filename = os.path.split(self.clean_filenames[tar_index])[-1]
+        noisy_filename = os.path.split(self.noisy_filenames[tar_index])[-1]
+        mask_filename = os.path.split(self.mask_filenames[tar_index])[-1]
+
+        clean = clean.permute(2,0,1)
+        noisy = noisy.permute(2,0,1)
+        mask = torch.unsqueeze(mask, dim=0)
+
+        return clean, noisy, mask, clean_filename, noisy_filename
+
+
+class DataLoaderTest(Dataset):
+    def __init__(self, rgb_dir, target_transform=None):
+        super(DataLoaderVal, self).__init__()
+
+        self.target_transform = target_transform
+
         gt_dir = 'test_C'
         input_dir = 'test_A'
         mask_dir = 'test_B'
